db6650a9b2bec56652f06cab8a31a14f35de20dc
Artur Nikytenko <arturnikytenko@gmail.com>
2024-03-11 21:33:57 +0100

    calculated average

--- apbd02/Program.cs
+++ apbd02/Program.cs
@@ -3,3 +3,13 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modification 1");
 Console.WriteLine("Modification 2");
 Console.WriteLine("Modification 3");
+
+static double Average(int[] arr)
+{
+    double average = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        average += arr[i];
+    }
+    return average/arr.Length;
+}
